e332cf61c90d405fcb273009283d8b364eeff3e2
Bucky <buckyroberts@gmail.com>
2020-05-13 12:34:25 -0700

    Adding trust field

--- v1/validators/models/validator.py
+++ v1/validators/models/validator.py
@@ -1,11 +1,21 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
 class Validator(models.Model):
-    ip_address = models.CharField(max_length=256)
+    ip_address = models.GenericIPAddressField()
+    trust = models.DecimalField(
+        decimal_places=2,
+        default=0,
+        max_digits=5,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100)
+        ]
+    )
 
     class Meta:
         default_related_name = 'validators'
 
     def __str__(self):
-        return f'{self.id} | {self.ip_address}'
+        return f'{self.id} | {self.ip_address} | {self.trust}'
