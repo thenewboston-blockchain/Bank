6e862f87eccd184e28d1435e2c7924f3729359a2
Bucky <buckyroberts@gmail.com>
2020-05-17 11:17:07 -0700

    Sync

--- v1/network/utils/format.py
+++ v1/network/utils/format.py
@@ -4,7 +4,7 @@ def format_address(*, ip_address, port, protocol):
     """
 
     port = f':{port}' if port else ''
-    return f'{protocol}://{ip_address}{port}/'
+    return f'{protocol}://{ip_address}{port}'
 
 
 def format_node_address(*, node):
@@ -15,4 +15,4 @@ def format_node_address(*, node):
     ip_address = node.ip_address
     port = f':{node.port}' if node.port else ''
     protocol = node.protocol
-    return f'{protocol}://{ip_address}{port}/'
+    return f'{protocol}://{ip_address}{port}'
