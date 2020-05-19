33d75a6883bfdeb5b51a62401e9e1267bd76acd5
Bucky <buckyroberts@gmail.com>
2020-05-19 15:41:50 -0700

    Sync

--- scripts/pull_to_sync_network_package.py
+++
@@ -1,7 +0,0 @@
-from distutils.dir_util import copy_tree
-
-from_dir = '/Users/bucky/Desktop/Projects/Validator/v1/network'
-to_dir = '/Users/bucky/Desktop/Projects/Bank/v1/network'
-
-if __name__ == '__main__':
-    copy_tree(from_dir, to_dir)
