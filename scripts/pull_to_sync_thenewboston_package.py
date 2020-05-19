b4d3b3318b791f8b1b49fc24908458fda703b220
Bucky <buckyroberts@gmail.com>
2020-05-19 11:00:19 -0700

    Checkpoint

--- scripts/pull_to_sync_thenewboston_package.py
+++
@@ -1,7 +0,0 @@
-from distutils.dir_util import copy_tree
-
-from_dir = '/Users/bucky/Desktop/Projects/thenewboston-python/thenewboston'
-to_dir = '/Users/bucky/Desktop/Projects/Bank/thenewboston'
-
-if __name__ == '__main__':
-    copy_tree(from_dir, to_dir)
