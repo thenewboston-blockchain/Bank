bec3f4b1ebcfc1814ce060d2234f38f0b8fc65a0
Bucky <buckyroberts@gmail.com>
2020-05-25 17:51:00 -0700

    Naming update

--- v1/tasks/blocks.py
+++ v1/tasks/blocks.py
@@ -1,14 +1,11 @@
 from celery import shared_task
-from celery.utils.log import get_task_logger
 from nacl.encoding import HexEncoder
 from nacl.signing import SigningKey
 from thenewboston.accounts.account_numbers import encode_account_number, get_account_number
-from thenewboston.blocks.signatures import generate_signature, verify_signature
+from thenewboston.blocks.signatures import generate_signature
 from thenewboston.utils.format import format_address
 from thenewboston.utils.network import post
 from thenewboston.utils.tools import sort_and_encode
-
-logger = get_task_logger(__name__)
 
 
 @shared_task
@@ -20,23 +17,16 @@ def sign_and_send_block(*, block, ip_address, port, protocol, url_path):
 
     # TODO: Signing key
     signing_key = SigningKey('e0ba29c1c493d01a5f665db55a4bd77caa140cf9722d0ed367ce4183230d2e02', encoder=HexEncoder)
-    account_number = get_account_number(signing_key=signing_key)
-    account_number = encode_account_number(account_number=account_number)
+    confirmation_identifier = get_account_number(signing_key=signing_key)
+    confirmation_identifier = encode_account_number(account_number=confirmation_identifier)
 
     message = sort_and_encode(block)
     signed_block = {
-        'account_number': account_number,
-        'signature': generate_signature(message=message, signing_key=signing_key),
-        'block': block
+        'block': block,
+        'confirmation_identifier': confirmation_identifier,
+        'signature': generate_signature(message=message, signing_key=signing_key)
     }
-
-    verify_signature(
-        account_number=account_number,
-        signature=signed_block['signature'],
-        message=message
-    )
 
     node_address = format_address(ip_address=ip_address, port=port, protocol=protocol)
     url = f'{node_address}{url_path}'
-    results = post(url=url, body=signed_block)
-    logger.warning(results)
+    post(url=url, body=signed_block)
