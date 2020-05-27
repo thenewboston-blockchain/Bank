3d6cb9099e9a424f8b49744f4a880c9f7b156ada
Bucky <buckyroberts@gmail.com>
2020-05-27 16:49:15 -0700

    Working on ConfirmationBlock validation

--- v1/blocks/serializers/confirmation_block.py
+++ v1/blocks/serializers/confirmation_block.py
@@ -1,8 +1,18 @@
+from django.db import transaction
 from rest_framework import serializers
-from thenewboston.constants.network import BLOCK_IDENTIFIER_LENGTH, SIGNATURE_LENGTH, VERIFY_KEY_LENGTH
+from thenewboston.blocks.signatures import verify_signature
+from thenewboston.constants.network import (
+    ACCEPTED,
+    BLOCK_IDENTIFIER_LENGTH,
+    PENDING,
+    SIGNATURE_LENGTH,
+    VERIFY_KEY_LENGTH
+)
 from thenewboston.serializers.block import BlockSerializer
 from thenewboston.utils.fields import all_field_names
 
+from v1.members.models.member import Member
+from v1.registrations.models.member_registration import MemberRegistration
 from v1.validators.models.validator import Validator
 from ..models.block import Block
 from ..models.confirmation_block import ConfirmationBlock
@@ -24,21 +34,42 @@ class ConfirmationBlockSerializerCreate(serializers.Serializer):
 
     def create(self, validated_data):
         """
-        Create
+        Create confirmation block
+        If the inner blocks account number relates to a pending MemberRegistration:
+        - create Member
+        - update MemberRegistration to accepted
         """
 
         block_identifier = validated_data['block_identifier']
-        inner_block = validated_data['block']
-        inner_block_signature = inner_block['signature']
         network_identifier = validated_data['network_identifier']
+
+        inner_block = validated_data['block']
+        inner_block_account_number = inner_block['account_number']
+        inner_block_signature = inner_block['signature']
 
         block = Block.objects.get(signature=inner_block_signature)
         validator = Validator.objects.get(network_identifier=network_identifier)
-        confirmation_block = ConfirmationBlock.objects.create(
-            block=block,
-            block_identifier=block_identifier,
-            validator=validator
-        )
+
+        try:
+            with transaction.atomic():
+                confirmation_block = ConfirmationBlock.objects.create(
+                    block=block,
+                    block_identifier=block_identifier,
+                    validator=validator
+                )
+                member_registration = MemberRegistration.objects.filter(
+                    account_number=inner_block_account_number,
+                    status=PENDING
+                )
+
+                if member_registration:
+                    member = Member.objects.create(
+                        account_number=inner_block_account_number,
+                        trust=50
+                    )
+                    member_registration.update(member=member, status=ACCEPTED)
+        except Exception as e:
+            print(e)
 
         return confirmation_block
 
@@ -47,10 +78,30 @@ class ConfirmationBlockSerializerCreate(serializers.Serializer):
 
     def validate(self, data):
         """
-        Validate
+        Validate signature
         """
 
-        # TODO: Check NID is from PV or BUV
-        # TODO: Validate signature from validator (PV or BUV)
+        block = data['block']
+        network_identifier = data['network_identifier']
+        signature = data['signature']
+
+        # TODO: May have to encode the block first
+
+        verify_signature(
+            message=block,
+            signature=signature,
+            verify_key=network_identifier
+        )
 
         return data
+
+    @staticmethod
+    def validate_network_identifier(network_identifier):
+        """
+        Validate that network_identifier belongs to validator
+        """
+
+        if not Validator.objects.filter(network_identifier=network_identifier).exists():
+            raise serializers.ValidationError('Validator with that network_identifier does not exist')
+
+        return network_identifier
