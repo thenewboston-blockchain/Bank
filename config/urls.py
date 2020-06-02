5de0f752eed5fad3f67f4e2ab20b737edfc04c73
Justin Kim <justinkim943@gmail.com>
2020-06-01 19:29:09 -0700

    Installed django-debug-toolbar

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -23,3 +24,10 @@ urlpatterns = [
     path('', include('v1.validators.urls')),
 
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
