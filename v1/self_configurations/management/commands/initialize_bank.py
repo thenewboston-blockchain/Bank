91445b007ae37b621474d8869116f46798087461
Bucky <buckyroberts@gmail.com>
2020-06-17 18:47:10 -0700

    Bank init script

---
+++ v1/self_configurations/management/commands/initialize_bank.py
@@ -0,0 +1,81 @@
+from thenewboston.base_classes.initialize_node import InitializeNode
+from thenewboston.constants.network import BANK
+
+from v1.banks.models.bank import Bank
+from v1.self_configurations.models.self_configuration import SelfConfiguration
+
+"""
+python3 manage.py initialize_bank
+
+Running this script will:
+- delete existing SelfConfiguration and related Bank objects
+- create SelfConfiguration and related Bank objects
+"""
+
+
+class Command(InitializeNode):
+    help = 'Initialize bank'
+
+    def __init__(self):
+        super().__init__()
+
+        self.primary_validator = None
+        self.required_input = {
+            'account_number': None,
+            'default_transaction_fee': None,
+            'ip_address': None,
+            'network_identifier': None,
+            'port': None,
+            'protocol': None,
+            'registration_fee': None,
+            'version': None
+        }
+
+    def handle(self, *args, **options):
+        """
+        Run script
+        """
+
+        # Input values
+        self.get_verify_key(
+            attribute_name='network_identifier',
+            human_readable_name='network identifier'
+        )
+        self.get_verify_key(
+            attribute_name='account_number',
+            human_readable_name='account number'
+        )
+        self.get_fee(
+            attribute_name='default_transaction_fee',
+            human_readable_name='default transaction fee'
+        )
+        self.get_fee(
+            attribute_name='registration_fee',
+            human_readable_name='registration fee'
+        )
+        self.get_protocol()
+        self.get_ip_address()
+        self.get_port()
+        self.get_version_number()
+
+        self.initialize_bank()
+
+    def initialize_bank(self):
+        """
+        Process to initialize bank:
+        - delete existing SelfConfiguration and related Bank objects
+        - create SelfConfiguration and related Bank objects
+        """
+
+        # Delete existing SelfConfiguration and related Bank objects
+        SelfConfiguration.objects.all().delete()
+        Bank.objects.filter(ip_address=self.required_input['ip_address']).delete()
+
+        # Create SelfConfiguration and related Bank objects
+        SelfConfiguration.objects.create(
+            **self.required_input,
+            node_type=BANK,
+            primary_validator=self.primary_validator
+        )
+
+        self.stdout.write(self.style.SUCCESS('Primary validator initialization complete'))
