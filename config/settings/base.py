c7101f0b7606fc51df628ab092d2c27e2f7e0b5b
Bucky <buckyroberts@gmail.com>
2020-06-18 20:50:12 -0700

    Add static dir

--- config/settings/base.py
+++ config/settings/base.py
@@ -93,6 +93,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
