b462063e43fd330decb38b13b8cfb7d2df776da5
Bucky <buckyroberts@gmail.com>
2020-06-19 20:10:16 -0700

    Dirty BankRegistrationSerializerCreate

--- v1/bank_registrations/README.md
+++ v1/bank_registrations/README.md
@@ -8,6 +8,35 @@ existing banks. Banks must also prove that they are configured properly to act a
 ability to act as a server by responding properly to network requests made from the validator to the bank's IP address. 
 This verification prevents end users from acting as banks by sending transactions directly to the validator.
 
+### POST /bank_registrations
+
+- `message` - `validator` to register with and `block` payment for registration fee
+- `network_identifier` - network identifier of the primary validator
+- `signature` - hex value of the signed `message`
+
+```
+{
+  "message": {
+    "block": {
+      "account_number": "5e12967707909e62b2bb2036c209085a784fabbc3deccefee70052b6181c8ed8",
+      "message": {
+        "balance_key": "5e12967707909e62b2bb2036c209085a784fabbc3deccefee70052b6181c8ed8",
+        "txs": [
+          {
+            "amount": 1,
+            "recipient": "ad1f8845c6a1abb6011a2a434a079a087c460657aad54329a84b406dce8bf314"
+          }
+        ]
+      },
+      "signature": "8216cb5d8ca2ff76a8b522f5c2e0e2090668e8a80224900e3c378b453e822666d58d314efa72f13bfdb6538d284edbde2b24f402aed00a89883af90a809c1d04"
+    },
+    "validator": "a8101b03-15ad-42fc-8e64-2de24b850e0e"
+  },
+  "network_identifier": "d5356888dc9303e44ce52b1e06c3165a7759b9df1e6a6dfbd33ee1c3df1ab4d1",
+  "signature": "f12452a689bd86ce36abda70d46df3cb97975315ccdfb39b4989b86191bf6e9cbe545174a14927dd0c2a96ded7e393d672e3eff95c36d53906a0afd85dcd000a"
+}
+```
+
 ### PATCH /bank_registrations
 
 - `message` - status to indicate the result of the bank registration
--- v1/bank_registrations/serializers/bank_registration.py
+++ v1/bank_registrations/serializers/bank_registration.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
+from thenewboston.blocks.signatures import verify_signature
+from thenewboston.constants.network import PENDING, VERIFY_KEY_LENGTH
+from thenewboston.serializers.network_block import NetworkBlockSerializer
+from thenewboston.transactions.validation import validate_transaction_exists
 from thenewboston.utils.fields import all_field_names
+from thenewboston.utils.tools import sort_and_encode
 
+from v1.validators.models.validator import Validator
 from ..models.bank_registration import BankRegistration
 
 
@@ -12,18 +18,79 @@ class BankRegistrationSerializer(serializers.ModelSerializer):
         read_only_fields = all_field_names(BankRegistration)
 
 
-class BankRegistrationSerializerCreate(serializers.ModelSerializer):
+class BankRegistrationSerializerCreate(serializers.Serializer):
+    block = NetworkBlockSerializer()
+    validator_network_identifier = serializers.CharField(max_length=VERIFY_KEY_LENGTH)
 
-    class Meta:
-        model = BankRegistration
-        exclude = ('status',)
+    def create(self, validated_data):
+        """
+        Create bank registration
+        Forward block to validator
+        """
+
+        block = validated_data['block']
+        validator = validated_data['validator']
+        print(block)
+
+        bank_registration = BankRegistration.objects.create(
+            fee=validator.registration_fee,
+            status=PENDING,
+            validator=validator
+        )
+
+        return bank_registration
+
+    def update(self, instance, validated_data):
+        raise RuntimeError('Method unavailable')
 
     def validate(self, data):
         """
-        Validate something
+        Validate block signature
+        Verify that correct payment exist for Validator registration fee
+
+        Note: when building the block, message is pulled from 'initial_data' since 'data' has already been processed by
+        the NetworkBlockSerializer converting all amounts to DecimalField (which are not JSON serializable)
         """
 
-        return data
+        block = self.initial_data['block']
+        validator = data['validator_network_identifier']
+
+        verify_signature(
+            message=sort_and_encode(block['message']),
+            signature=block['signature'],
+            verify_key=block['account_number']
+        )
+
+        validate_transaction_exists(
+            amount=validator.registration_fee,
+            error=serializers.ValidationError,
+            recipient=validator.account_number,
+            txs=block['message']['txs']
+        )
+
+        return {
+            'block': block,
+            'validator': validator
+        }
+
+    @staticmethod
+    def validate_validator_network_identifier(validator_network_identifier):
+        """
+        Ensure validator_network_identifier matches known validator NID
+        Check for existing pending registration
+        """
+
+        validator = Validator.objects.filter(network_identifier=validator_network_identifier).first()
+
+        if not validator:
+            raise serializers.ValidationError(
+                f'Could not find validator with validator_network_identifier {validator_network_identifier}'
+            )
+
+        if BankRegistration.objects.filter(status=PENDING, validator=validator).exists():
+            raise serializers.ValidationError('Pending registration already exists')
+
+        return validator
 
 
 class BankRegistrationSerializerUpdate(serializers.ModelSerializer):
