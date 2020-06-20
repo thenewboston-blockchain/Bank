7412d293192c42bec51f72a36c1ed1f3f5ccdf41
Bucky <buckyroberts@gmail.com>
2020-06-20 13:13:22 -0700

    Adding original_bank_registration_pk for PATCH response

--- v1/bank_registrations/serializers/bank_registration.py
+++ v1/bank_registrations/serializers/bank_registration.py
@@ -46,6 +46,7 @@ class BankRegistrationSerializerCreate(serializers.Serializer):
                     data={
                         'block': block,
                         'ip_address': self_configuration.ip_address,
+                        'original_bank_registration_pk': bank_registration.pk,
                         'port': self_configuration.port,
                         'protocol': self_configuration.protocol,
                         'validator_network_identifier': validator.network_identifier,
