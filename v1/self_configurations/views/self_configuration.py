c78d7b012892be057df299c3b7cb1c3649a21bd1
Bucky <buckyroberts@gmail.com>
2020-06-20 20:22:21 -0700

    Test

--- v1/self_configurations/views/self_configuration.py
+++ v1/self_configurations/views/self_configuration.py
@@ -1,8 +1,12 @@
+import logging
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from ..helpers.self_configuration import get_self_configuration
 from ..serializers.self_configuration import SelfConfigurationSerializer
+
+logger = logging.getLogger('thenewboston')
 
 
 # config
@@ -13,6 +17,12 @@ class SelfConfigurationDetail(APIView):
         """
         description: Get self configuration details
         """
+
+        try:
+            x = 1 / 0
+            print(x)
+        except Exception as e:
+            logger.exception(e)
 
         self_configuration = get_self_configuration(exception_class=RuntimeError)
         return Response(SelfConfigurationSerializer(self_configuration).data)
