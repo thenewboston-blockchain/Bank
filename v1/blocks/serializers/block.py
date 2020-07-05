0b991fcde96de24cd2b6250413d5514f4fa8cbdc
Bucky <buckyroberts@gmail.com>
2020-07-05 13:14:10 -0700

    Cleaning

--- v1/blocks/serializers/block.py
+++ v1/blocks/serializers/block.py
@@ -8,7 +8,6 @@ from thenewboston.transactions.validation import validate_transaction_exists
 from thenewboston.utils.fields import all_field_names
 from thenewboston.utils.tools import sort_and_encode
 
-from v1.accounts.models.account import Account
 from v1.self_configurations.helpers.self_configuration import get_self_configuration
 from v1.tasks.blocks import send_signed_block
 from v1.utils.blocks import create_block_and_bank_transactions
@@ -76,17 +75,6 @@ class BlockSerializerCreate(NetworkBlockSerializer):
             verify_key=block['account_number']
         )
         return block
-
-    @staticmethod
-    def validate_account_number(account_number):
-        """
-        Check account number belongs to a registered account
-        """
-
-        if not Account.objects.filter(account_number=account_number).exists():
-            raise serializers.ValidationError('Account with that account number does not exist')
-
-        return account_number
 
     @staticmethod
     def validate_message(message):
