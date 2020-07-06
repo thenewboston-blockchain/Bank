9e16ebe0c91ced92bce143c28152e0f992c8d5f6
Bucky <buckyroberts@gmail.com>
2020-07-05 18:51:35 -0700

    Create account on block creation

--- v1/accounts/models/account.py
+++ v1/accounts/models/account.py
@@ -2,24 +2,13 @@ from uuid import uuid4
 
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from thenewboston.constants.network import BALANCE_LOCK_LENGTH, VERIFY_KEY_LENGTH
+from thenewboston.constants.network import VERIFY_KEY_LENGTH
 from thenewboston.models.created_modified import CreatedModified
-from thenewboston.utils.validators import validate_is_real_number
 
 
 class Account(CreatedModified):
     id = models.UUIDField(default=uuid4, editable=False, primary_key=True)
     account_number = models.CharField(max_length=VERIFY_KEY_LENGTH, unique=True)
-    balance = models.DecimalField(
-        decimal_places=16,
-        default=0,
-        max_digits=32,
-        validators=[
-            MinValueValidator(0),
-            validate_is_real_number
-        ]
-    )
-    balance_lock = models.CharField(max_length=BALANCE_LOCK_LENGTH, unique=True)
     trust = models.DecimalField(
         decimal_places=2,
         default=0,
@@ -36,7 +25,5 @@ class Account(CreatedModified):
     def __str__(self):
         return (
             f'Account Number: {self.account_number} | '
-            f'Balance: {self.balance} | '
-            f'Balance Lock: {self.balance_lock} | '
             f'Trust: {self.trust}'
         )
--- v1/blocks/serializers/block.py
+++ v1/blocks/serializers/block.py
@@ -10,6 +10,7 @@ from thenewboston.utils.tools import sort_and_encode
 
 from v1.self_configurations.helpers.self_configuration import get_self_configuration
 from v1.tasks.blocks import send_signed_block
+from v1.accounts.models.account import Account
 from v1.utils.blocks import create_block_and_bank_transactions
 from v1.validators.helpers.validator_configuration import get_primary_validator
 from ..models.block import Block
@@ -40,6 +41,10 @@ class BlockSerializerCreate(NetworkBlockSerializer):
         try:
             with transaction.atomic():
                 block = create_block_and_bank_transactions(validated_block)
+                Account.objects.get_or_create(
+                    account_number=validated_block['account_number'],
+                    defaults={'trust': 0},
+                )
                 send_signed_block.delay(
                     block=validated_block,
                     ip_address=primary_validator.ip_address,
