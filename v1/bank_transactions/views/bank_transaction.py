71db9767e9d1580343185e740b20736906c74e14
Bucky Roberts <buckyroberts@gmail.com>
2020-07-19 13:50:23 -0700

    Bank transactions now send back related block (#13)

--- v1/bank_transactions/views/bank_transaction.py
+++ v1/bank_transactions/views/bank_transaction.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from ..helpers.optimizations import optimize_bank_transaction_list
 from ..models.bank_transaction import BankTransaction
 from ..serializers.bank_transaction import BankTransactionSerializer
 
@@ -15,4 +16,5 @@ class BankTransactionView(APIView):
         """
 
         bank_transactions = BankTransaction.objects.all()
+        bank_transactions = optimize_bank_transaction_list(bank_transactions)
         return Response(BankTransactionSerializer(bank_transactions, many=True).data)
