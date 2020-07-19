e9ddbee1ef82d1a69cae280649d98128cd682b27
Bucky <buckyroberts@gmail.com>
2020-07-19 13:23:36 -0700

    Bank transactions now send back related block

---
+++ v1/bank_transactions/helpers/optimizations.py
@@ -0,0 +1,6 @@
+def optimize_bank_transaction_list(bank_transactions):
+    """
+    Append related objects using select_related and prefetch_related
+    """
+
+    return bank_transactions.select_related('block')
