03e576b149301147547b1ffabdbf91ca52da9ea5
Bucky <buckyroberts@gmail.com>
2020-07-20 17:18:41 -0700

    Ticket complete

--- v1/validators/urls.py
+++ v1/validators/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views.validator import ValidatorView
+from .views.validator import ValidatorDetail, ValidatorView
 
 urlpatterns = [
 
     # Validators
     path('validators', ValidatorView.as_view()),
+    path('validators/<str:node_identifier>', ValidatorDetail.as_view()),
 
 ]
--- v1/validators/views/validator.py
+++ v1/validators/views/validator.py
@@ -1,8 +1,11 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from v1.decorators.nodes import is_self_signed_message
 from ..models.validator import Validator
-from ..serializers.validator import ValidatorSerializer
+from ..serializers.validator import ValidatorSerializer, ValidatorSerializerUpdate
 
 
 # validators
@@ -16,3 +19,28 @@ class ValidatorView(APIView):
 
         validators = Validator.objects.all()
         return Response(ValidatorSerializer(validators, many=True).data)
+
+
+# validators/{node_identifier}
+class ValidatorDetail(APIView):
+
+    @staticmethod
+    @is_self_signed_message
+    def patch(request, node_identifier):
+        """
+        description: Update validator
+        parameters:
+          - name: trust
+            type: number
+        """
+
+        validator = get_object_or_404(Validator, node_identifier=node_identifier)
+        serializer = ValidatorSerializerUpdate(
+            validator,
+            data=request.data['message'],
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(ValidatorSerializer(serializer.instance).data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
