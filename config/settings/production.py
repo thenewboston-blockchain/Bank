3098d43feea83494fdc08303f0662963af898283
Bucky Roberts <buckyroberts@gmail.com>
2020-07-21 21:02:06 -0700

    Production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,3 +1,24 @@
 from .base import *
 
-DEBUG = False
+DEBUG = True
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
+INSTALLED_APPS.append('debug_toolbar')
+
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+
+CELERY_BROKER_URL = 'redis://localhost'
+CELERY_RESULT_BACKEND = 'redis://localhost'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
