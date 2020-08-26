e2e3a903c83ce720ccd02dcd4863b2f22f2fdb08
Bucky <buckyroberts@gmail.com>
2020-08-26 13:34:33 -0700

    Fix typo

--- v1/status_updates/tests/upgrade_notice.py
+++ v1/status_updates/tests/upgrade_notice.py
@@ -3,7 +3,7 @@ from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from thenewboston.utils.signed_requests import generate_signed_request
 
 
-def set_primary_vaidator(client, self_configuration, signing_key, status):
+def set_primary_validator(client, self_configuration, signing_key, status):
 
     return client.post_json(
         reverse('upgrade_notice-list'),
@@ -21,20 +21,20 @@ def test_upgrade_notice_200(client, self_configuration, validator, signing_key, 
     validator.trust = self_configuration.primary_validator.trust + 1
     validator.save()
 
-    assert set_primary_vaidator(client, self_configuration, signing_key, HTTP_200_OK) == {}
+    assert set_primary_validator(client, self_configuration, signing_key, HTTP_200_OK) == {}
 
 
 def test_upgrade_notice_200_same_validator(client, self_configuration, validator, signing_key, celery_worker):
     validator.trust = self_configuration.primary_validator.trust + 1
     validator.save()
 
-    set_primary_vaidator(client, self_configuration, signing_key, HTTP_200_OK)
-    set_primary_vaidator(client, self_configuration, signing_key, HTTP_200_OK)
+    set_primary_validator(client, self_configuration, signing_key, HTTP_200_OK)
+    set_primary_validator(client, self_configuration, signing_key, HTTP_200_OK)
 
 
 def test_upgrade_notice_400_low_trust(client, self_configuration, validator, signing_key, celery_worker):
     validator.trust = self_configuration.primary_validator.trust - 1
     validator.save()
 
-    response = set_primary_vaidator(client, self_configuration, signing_key, HTTP_400_BAD_REQUEST)
+    response = set_primary_validator(client, self_configuration, signing_key, HTTP_400_BAD_REQUEST)
     assert response == ['Networks out of sync']
