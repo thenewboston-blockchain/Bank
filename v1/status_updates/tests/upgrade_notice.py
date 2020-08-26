7f874051e7fef73507dc3d4d16d71690da8377a7
Mikhail <mgponomarev@gmail.com>
2020-08-26 03:03:53 +0300

    resync endpoints testing

--- v1/status_updates/tests/upgrade_notice.py
+++ v1/status_updates/tests/upgrade_notice.py
@@ -1,13 +1,11 @@
 from rest_framework.reverse import reverse
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from thenewboston.utils.signed_requests import generate_signed_request
 
 
-def test_upgrade_notice_post(client, self_configuration, validator, signing_key):
-    validator.trust = self_configuration.primary_validator.trust + 1
-    validator.save()
+def set_primary_vaidator(client, self_configuration, signing_key, status):
 
-    client.post_json(
+    return client.post_json(
         reverse('upgrade_notice-list'),
         generate_signed_request(
             data={
@@ -15,5 +13,28 @@ def test_upgrade_notice_post(client, self_configuration, validator, signing_key)
             },
             nid_signing_key=signing_key
         ),
-        expected=HTTP_200_OK
+        expected=status
     )
+
+
+def test_upgrade_notice_200(client, self_configuration, validator, signing_key, celery_worker):
+    validator.trust = self_configuration.primary_validator.trust + 1
+    validator.save()
+
+    assert set_primary_vaidator(client, self_configuration, signing_key, HTTP_200_OK) == {}
+
+
+def test_upgrade_notice_200_same_validator(client, self_configuration, validator, signing_key, celery_worker):
+    validator.trust = self_configuration.primary_validator.trust + 1
+    validator.save()
+
+    set_primary_vaidator(client, self_configuration, signing_key, HTTP_200_OK)
+    set_primary_vaidator(client, self_configuration, signing_key, HTTP_200_OK)
+
+
+def test_upgrade_notice_400_low_trust(client, self_configuration, validator, signing_key, celery_worker):
+    validator.trust = self_configuration.primary_validator.trust - 1
+    validator.save()
+
+    response = set_primary_vaidator(client, self_configuration, signing_key, HTTP_400_BAD_REQUEST)
+    assert response == ['Networks out of sync']
