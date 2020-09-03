a38e1389e21521ecb1b4dd22d07efb424a301485
Bucky <buckyroberts@gmail.com>
2020-09-03 16:18:11 -0700

    Bank block forwarding

--- v1/utils/blocks.py
+++ v1/utils/blocks.py
@@ -1,22 +1,16 @@
+from rest_framework import serializers
+
 from v1.bank_transactions.models.bank_transaction import BankTransaction
 from v1.blocks.models.block import Block
+from v1.confirmation_blocks.models.confirmation_block import ConfirmationBlock
 
 
-def create_block_and_bank_transactions(block_data):
+def create_bank_transactions(*, block, message):
     """
-    Create block and bank transactions
+    Crete bank transactions from given block data
     """
-
-    message = block_data['message']
-
-    block = Block.objects.create(
-        balance_key=message['balance_key'],
-        sender=block_data['account_number'],
-        signature=block_data['signature']
-    )
 
     bank_transactions = []
-    message = block_data['message']
 
     for tx in message['txs']:
         bank_transaction = BankTransaction(
@@ -28,4 +22,40 @@ def create_block_and_bank_transactions(block_data):
 
     BankTransaction.objects.bulk_create(bank_transactions)
 
-    return block
+
+def create_block_and_bank_transactions(block_data):
+    """
+    Create block and bank transactions
+    Returns block, created
+    """
+
+    account_number = block_data['account_number']
+    message = block_data['message']
+    signature = block_data['signature']
+    balance_key = message['balance_key']
+
+    block = Block.objects.filter(balance_key=balance_key).first()
+
+    if block:
+
+        # User is attempting to resend the same exact block
+        if block.signature == signature:
+
+            if ConfirmationBlock.objects.filter(block=block).exists():
+                raise serializers.ValidationError('Block has already been confirmed')
+
+            return block, False
+
+        # User is using that balance key to send a new block (different Txs)
+        BankTransaction.objects.filter(block=block).delete()
+        create_bank_transactions(block=block, message=message)
+        return block, False
+
+    block = Block.objects.create(
+        balance_key=balance_key,
+        sender=account_number,
+        signature=signature
+    )
+    create_bank_transactions(block=block, message=message)
+
+    return block, True
