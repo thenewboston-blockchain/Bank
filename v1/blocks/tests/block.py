f67db39334638ae94a81d278fa3398172b5cfee0
Mikhail <5008719+fonar@users.noreply.github.com>
2020-09-04 00:07:27 +0300

    testing for block recepients to be unique (#44)

--- v1/blocks/tests/block.py
+++ v1/blocks/tests/block.py
@@ -1,7 +1,7 @@
 import random
 
 from rest_framework.reverse import reverse
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from thenewboston.third_party.pytest.asserts import assert_objects_vs_dicts
 
 
@@ -41,3 +41,13 @@ def test_blocks_post(client, block_data):
     )
     assert block_data['signature'] == response['signature']
     assert block_data['message']['balance_key'] == response['balance_key']
+
+
+def test_blocks_post_400_unique_recipients(client, block_data_unique_recipients):
+
+    response = client.post_json(
+        reverse('block-list'),
+        block_data_unique_recipients,
+        expected=HTTP_400_BAD_REQUEST,
+    )
+    assert response['non_field_errors'] == ["Tx recipients must be unique"]
