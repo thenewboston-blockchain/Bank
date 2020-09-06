3cd6274e0b586a4863833165a980c430a768be8b
Bucky <buckyroberts@gmail.com>
2020-09-05 19:37:23 -0700

    Fix import

--- v1/crawl/routing.py
+++ v1/crawl/routing.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
 
-from .consumers.crawl import CrawlStatusConsumer
+from .consumers.crawl_status import CrawlStatusConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/crawl_status$', CrawlStatusConsumer),
