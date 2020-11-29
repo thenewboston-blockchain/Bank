5c2d0acc811e86a093799b0541ec7e318238f59c
Bucky Roberts <buckyroberts@gmail.com>
2020-11-28 19:42:59 -0800

    Add media (#106)
    * Add .gitkeep
    
    * Ignore .gitkeep

--- config/settings/base.py
+++ config/settings/base.py
@@ -105,6 +105,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
