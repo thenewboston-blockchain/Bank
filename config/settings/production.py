548fda56161dcf010104f574ce7ed575dff41acf
bucky <buckyroberts@gmail.com>
2020-11-28 19:36:35 -0800

    Add .gitkeep

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from .base import *
 
-
 SENTRY_DSN = os.getenv('SENTRY_DSN')
+
 if SENTRY_DSN:
     import sentry_sdk
     from sentry_sdk.integrations.celery import CeleryIntegration
     from sentry_sdk.integrations.django import DjangoIntegration
     from sentry_sdk.integrations.redis import RedisIntegration
     from sentry_sdk.integrations.tornado import TornadoIntegration
+
     sentry_sdk.init(
         SENTRY_DSN,
         traces_sample_rate=1.0,
