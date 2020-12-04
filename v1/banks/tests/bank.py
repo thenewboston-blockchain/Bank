b3da28f9c44c0f92adbed4332df79a12e92801e9
Duc Pham <phamcongduc1994@gmail.com>
2020-12-04 22:58:56 +0700

    Write a test to validate created bank trust value

--- v1/banks/tests/bank.py
+++ v1/banks/tests/bank.py
@@ -52,7 +52,29 @@ def test_banks_patch(client, bank, bank_fake_data, self_configuration):
      (-1, 'Ensure this value is greater than or equal to 0.'),
      (192, 'Ensure this value is less than or equal to 100.')]
 )
-def test_invalid_bank_trust_value(
+def test_create_bank_with_invalid_trust_value(
+    client, bank, bank_fake_data, self_configuration, trust, response_msg
+):
+    bank_fake_data['trust'] = trust
+    response = client.post_json(
+        reverse('bank-list'),
+        generate_signed_request(
+            data=bank_fake_data,
+            nid_signing_key=get_signing_key(),
+        ),
+        expected=status.HTTP_400_BAD_REQUEST,
+    )
+
+    assert response['trust'] == [response_msg]
+
+
+@pytest.mark.parametrize(
+    'trust, response_msg',
+    [(55.178, 'Ensure that there are no more than 2 decimal places.'),
+     (-1, 'Ensure this value is greater than or equal to 0.'),
+     (192, 'Ensure this value is less than or equal to 100.')]
+)
+def test_update_bank_with_invalid_trust_value(
     client, bank, bank_fake_data, self_configuration, trust, response_msg
 ):
     bank_fake_data['trust'] = trust
