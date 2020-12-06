39d598b492abc8fd67031da3b8de85f8080e9a1a
Jason Loo <loo.jason95@gmail.com>
2020-12-05 17:28:26 -0800

    Add test_validator_confirmation_service (#111)

--- v1/validator_confirmation_services/tests/validator_confirmation_service.py
+++ v1/validator_confirmation_services/tests/validator_confirmation_service.py
@@ -3,7 +3,21 @@ from datetime import datetime, timedelta
 
 from rest_framework.reverse import reverse
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
+from thenewboston.third_party.pytest.asserts import assert_objects_vs_dicts
 from thenewboston.utils.signed_requests import generate_signed_request
+
+
+def test_validator_confirmation_service_list(
+    client, validator_confirmation_services, django_assert_max_num_queries,
+):
+    with django_assert_max_num_queries(2):
+        response = client.get_json(
+            reverse('validatorconfirmationservice-list'),
+            {'limit': 0},
+            expected=HTTP_200_OK,
+        )
+    assert_objects_vs_dicts(validator_confirmation_services, response)
+    assert response
 
 
 def test_validator_confirmation_service_filter(
