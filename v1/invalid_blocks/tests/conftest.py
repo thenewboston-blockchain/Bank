e6a4fef0967bde8dfd2e6bd93b52d8bbee37e885
Duc Pham <phamcongduc1994@gmail.com>
2020-12-07 22:45:11 +0700

    Write a test to validate unique constraint for InvalidBlock

--- v1/invalid_blocks/tests/conftest.py
+++ v1/invalid_blocks/tests/conftest.py
@@ -19,6 +19,18 @@ def invalid_block_data(block_data, signing_key, invalid_block_fake_data):
 
 
 @pytest.fixture
+def invalid_block_data_unique_recipients(block_data_unique_recipients, signing_key, invalid_block_fake_data):
+    yield generate_signed_request(
+        data={
+            'block': block_data_unique_recipients,
+            'block_identifier': invalid_block_fake_data['block_identifier'],
+            'primary_validator_node_identifier': get_primary_validator().node_identifier,
+        },
+        nid_signing_key=signing_key
+    )
+
+
+@pytest.fixture
 def invalid_block_fake_data():
     yield build_json(InvalidBlockFactory)
 
