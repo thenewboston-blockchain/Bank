5d5cef9c67a115b3d9c14b280e242cc0f64c68a0
DucPhamTV <phamcongduc1994@gmail.com>
2020-12-10 23:19:53 +0000

    Write a test to validate unique constraint for InvalidBlock (#122)

--- v1/invalid_blocks/tests/invalid_block.py
+++ v1/invalid_blocks/tests/invalid_block.py
@@ -1,5 +1,5 @@
 from rest_framework.reverse import reverse
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from thenewboston.third_party.pytest.asserts import assert_objects_vs_dicts
 
 
@@ -23,3 +23,14 @@ def test_invalid_block_post(client, validator, block_data, block, invalid_block_
         expected=HTTP_201_CREATED,
     )
     assert invalid_block_data['message']['block_identifier'] == response['block_identifier']
+
+
+def test_invalid_block_post_400_unique_recipients(
+    client, validator, block, invalid_block_data_unique_recipients
+):
+    response = client.post_json(
+        reverse('invalidblock-list'),
+        invalid_block_data_unique_recipients,
+        expected=HTTP_400_BAD_REQUEST,
+    )
+    assert response['message']['block']['non_field_errors'] == ['Tx recipients must be unique']
