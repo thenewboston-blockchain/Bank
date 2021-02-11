1320d00c5998a2ccb0befbc5a0f69b1af8503e14
danpadev <daniel.pantja1991@gmail.com>
2021-02-11 19:35:32 +0000

    Add conditional if when creating bank_transaction
    A bank transaction should only be created when account number
    does not match recipient in Bank fee (do not pay a fee to yourself
    if you own the Bank node)

--- v1/utils/blocks.py
+++ v1/utils/blocks.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
+from thenewboston.constants.network import BANK
 from v1.accounts.models.account import Account
 from v1.bank_transactions.models.bank_transaction import BankTransaction
 from v1.blocks.models.block import Block
 from v1.confirmation_blocks.models.confirmation_block import ConfirmationBlock
+from v1.self_configurations.helpers.self_configuration import get_self_configuration
 
 
 def create_bank_transactions(*, block, message):
@@ -31,6 +33,8 @@ def create_block_and_related_objects(block_data):
     message = block_data['message']
     signature = block_data['signature']
     balance_key = message['balance_key']
+    txs = message['txs']
+    self_configuration = get_self_configuration(exception_class=RuntimeError)
 
     block = Block.objects.filter(balance_key=balance_key).first()
 
@@ -54,7 +58,12 @@ def create_block_and_related_objects(block_data):
         sender=account_number,
         signature=signature
     )
-    create_bank_transactions(block=block, message=message)
+
+    bank_fee = next(tx for tx in txs if tx['fee'] == BANK)
+    if bank_fee['recipient'] != self_configuration.account_number:
+        # User (sender) is owner of this Bank node. No need to pay the BANK fee to itself
+        create_bank_transactions(block=block, message=message)
+
     Account.objects.get_or_create(
         account_number=account_number,
         defaults={'trust': 0},
