bfbfec798725f7fb55273622fb56a42b213a8dde
Hussu <55182298+hussu010@users.noreply.github.com>
2021-07-06 09:29:46 +0545

    feat: block filter on confirmation_block api (#148)
    * feat: block filter on confirmation_block api
    
    * Remove unused import
    
    Co-authored-by: dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
    Co-authored-by: Bucky <buckyroberts@gmail.com>

---
+++ v1/confirmation_blocks/filters/confirmation_block.py
@@ -0,0 +1,12 @@
+from django_filters.rest_framework import FilterSet
+
+from ..models.confirmation_block import ConfirmationBlock
+
+
+class ConfirmationBlockFilter(FilterSet):
+
+    class Meta:
+        model = ConfirmationBlock
+        fields = [
+            'block'
+        ]
